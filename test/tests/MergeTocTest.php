94c18495fb169c9bb036ee87613cbb730b7291aa
Michele Locati <michele@locati.it>
2016-05-05 18:02:55 +0200

    Add tests for merging TOCs

---
+++ test/tests/MergeTocTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CHMLib\Test;
+
+use CHMLib\CHM;
+use CHMLib\Map;
+use CHMLib\TOCIndex\Tree;
+
+class MergeTocTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMergeToc()
+    {
+        $dir = dirname(dirname(__FILE__)).'/samples';
+        $main = CHM::fromFile($dir.'/main.chm');
+        $map = new Map();
+        $map->add('second.chm', CHM::fromFile($dir.'/second.chm'));
+        $toc = $main->getTOC();
+        $this->assertNotNull($toc);
+        $this->assertSame(
+            <<<EOT
+Heading in main
+    Topic in main
+second.chm>/second.hhc
+EOT
+            ,
+            rtrim(static::formatTree($toc, 0))
+        );
+        $toc->resolve($map);
+        $this->assertSame(<<<EOT
+Heading in main
+    Topic in main
+Heading in second
+    Topic in second
+EOT
+            ,
+            rtrim(static::formatTree($toc, 0))
+        );
+    }
+
+    protected static function formatTree(Tree $tree, $depth)
+    {
+        $result = '';
+        foreach ($tree->getItems() as $child) {
+            $result .= str_repeat('    ', $depth);
+            if ($child->getName() !== '') {
+                $result .= $child->getName();
+            } elseif ($child->getURL() !== '') {
+                $result .= $child->getURL();
+            } elseif (is_array($child->getMerge())) {
+                $result .= implode('>', $child->getMerge());
+            } elseif (is_string($child->getMerge())) {
+                $result .= $child->getMerge();
+            } else {
+                $result .= '?';
+            }
+            $result .= "\n";
+            $result .= static::formatTree($child->getChildren(), $depth + 1);
+        }
+
+        return $result;
+    }
+}
