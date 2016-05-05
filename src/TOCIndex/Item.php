16c279d6401fbfbcca02cc4c3425bf8e7ad1b393
Michele Locati <michele@locati.it>
2016-05-05 15:38:36 +0200

    Less choosy parser of index files
    Let's correctly parse .hhc/.hhk files even if they contain highly malformed "html" ;)

--- src/TOCIndex/Item.php
+++ src/TOCIndex/Item.php
@@ -34,11 +34,25 @@ class Item
     protected $keyword;
 
     /**
+     * The value of the "See Also" parameter.
+     *
+     * @var string
+     */
+    protected $seeAlso;
+
+    /**
      * The local path to the tree item.
      *
      * @var string
      */
     protected $local;
+
+    /**
+     * The URL to the tree item.
+     *
+     * @var string
+     */
+    protected $url;
 
     /**
      * The path to an entry in another CHM file.
@@ -57,24 +71,70 @@ class Item
     /**
      * The sub-elements of this Item.
      *
-     * @var Tree|null
+     * @var Tree
      */
-    protected $subTree;
+    protected $children;
 
     /**
      * Initializes the instance.
      *
      * @param CHM $chm The parent CHM instance.
+     * @param DOMElement $object The OBJECT element.
+     *
+     * @throws Exception Throw an Exception in case of errors.
+     *
+     * @return static
      */
-    protected function __construct(CHM $chm)
+    public function __construct(CHM $chm, DOMElement $object)
     {
         $this->chm = $chm;
         $this->name = '';
         $this->keyword = '';
+        $this->seeAlso = '';
         $this->local = '';
+        $this->url = '';
         $this->merge = null;
         $this->imageNumber = null;
-        $this->subTree = null;
+        $this->children = new Tree();
+        foreach ($object->childNodes as $p) {
+            if ($p instanceof DOMElement && strcasecmp($p->tagName, 'param') === 0) {
+                $name = trim((string) $p->getAttribute('name'));
+                $value = trim((string) $p->getAttribute('value'));
+                switch (strtolower($name)) {
+                    case 'name':
+                        // Multiple values are allowed: we keep only the last one
+                        $this->name = $value;
+                        break;
+                    case 'keyword':
+                        $this->keyword = $value;
+                        break;
+                    case 'see also':
+                        $this->seeAlso = $value;
+                        break;
+                    case 'local':
+                        $this->local = '/'.str_replace('\\', '/', $value);
+                        break;
+                    case 'url':
+                        $this->url = $value;
+                        break;
+                    case 'merge':
+                        if (!preg_match('%^([^:\\\\/]+)::(.+\.hh[ck])$%i', $value, $matches)) {
+                            throw new Exception("Invalid value of the '$name' attribute: $value");
+                        }
+                        $this->merge = array('chm' => $matches[1], 'entry' => '/'.ltrim(str_replace('\\', '/', $matches[2]), '/'));
+                        break;
+                    case 'imagenumber':
+                        if (is_numeric($value)) {
+                            $this->imageNumber = (int) $value;
+                        } elseif ($value !== '') {
+                            throw new Exception("Invalid value of the '$name' attribute: $value");
+                        }
+                        break;
+                    default:
+                        throw new Exception("Unknown parameter name '$name' of a tree item (value: '$value')");
+                }
+            }
+        }
     }
 
     /**
@@ -98,6 +158,16 @@ class Item
     }
 
     /**
+     * Get the value of the "See Also" parameter.
+     *
+     * @return string
+     */
+    public function getSeeAlso()
+    {
+        return $this->seeAlso;
+    }
+
+    /**
      * Get the local path to the tree item.
      *
      * @return string
@@ -105,6 +175,16 @@ class Item
     public function getLocal()
     {
         return $this->local;
+    }
+
+    /**
+     * Get the URL to the tree item.
+     *
+     * @return string
+     */
+    public function getURL()
+    {
+        return $this->url;
     }
 
     /**
@@ -130,11 +210,11 @@ class Item
     /**
      * Get the sub-elements of this Item.
      *
-     * @return Tree|null
+     * @return Tree
      */
-    public function getSubTree()
+    public function getChildren()
     {
-        return $this->subTree;
+        return $this->children;
     }
 
     /**
@@ -162,98 +242,6 @@ class Item
             $tree = Tree::fromString($chm, $entry->getContents());
             $tree->resolve($map);
             $result = $tree->getItems();
-        }
-
-        return $result;
-    }
-
-    /**
-     * Create a new instance starting from a LI element.
-     *
-     * @param CHM $chm The parent CHM instance.
-     * @param DOMElement $li
-     *
-     * @throws Exception Throw an Exception in case of errors.
-     *
-     * @return static
-     */
-    public static function fromLI(CHM $chm, DOMElement $li)
-    {
-        $result = array();
-        $itemForUL = null;
-        foreach ($li->childNodes as $c) {
-            if ($c instanceof DOMElement) {
-                switch (strtolower($c->tagName)) {
-                    case 'object':
-                        if (strcasecmp((string) $c->getAttribute('type'), 'text/sitemap') === 0) {
-                            $itemForUL = static::fromObject($chm, $c);
-                            $result[] = $itemForUL;
-                        }
-                        break;
-                    case 'ul':
-                        if ($itemForUL === null) {
-                            throw new Exception('No sitemap object found for a tree item');
-                        }
-                        $itemForUL->subTree = Tree::fromUL($chm, $c);
-                        $itemForUL = null;
-                        break;
-                }
-            }
-        }
-        if (empty($result)) {
-            throw new Exception('No sitemap object found for a tree item');
-        }
-
-        return $result;
-    }
-
-    /**
-     * Create a new instance starting from a LI element or from an OBJECT.
-     *
-     * @param CHM $chm The parent CHM instance.
-     * @param DOMElement $object
-     *
-     * @throws Exception Throw an Exception in case of errors.
-     *
-     * @return static
-     */
-    public static function fromObject(CHM $chm, DOMElement $object)
-    {
-        $result = new static($chm);
-        foreach ($object->childNodes as $p) {
-            if ($p instanceof DOMElement && strcasecmp($p->tagName, 'param') === 0) {
-                $name = trim((string) $p->getAttribute('name'));
-                $value = trim((string) $p->getAttribute('value'));
-                switch (strtolower($name)) {
-                    case 'name':
-                        $result->name = $value;
-                        break;
-                    case 'keyword':
-                        $result->keyword = $value;
-                        break;
-                    case 'local':
-                        $result->local = '/'.str_replace('\\', '/', $value);
-                        break;
-                    case 'merge':
-                        if (!preg_match('%^([^:\\\\/]+)::(.+\.hh[ck])$%i', $value, $matches)) {
-                            throw new Exception("Invalid value of the '$name' attribute: $value");
-                        }
-                        $result->merge = array('chm' => $matches[1], 'entry' => '/'.ltrim(str_replace('\\', '/', $matches[2]), '/'));
-                        break;
-                    case 'imagenumber':
-                        if (is_numeric($value)) {
-                            $result->imageNumber = (int) $value;
-                        } elseif ($value !== '') {
-                            throw new Exception("Invalid value of the '$name' attribute: $value");
-                        }
-                        break;
-                    default:
-                        throw new Exception("Unknown parameter name '$name' of a tree item (value: '$value')");
-                }
-            }
-        }
-        if ($result->name === 'dlg_vector_image') {
-            $result->name = 'dlg_vector_image';
         }
 
         return $result;
--- src/TOCIndex/Tree.php
+++ src/TOCIndex/Tree.php
@@ -15,13 +15,6 @@ use Exception;
 class Tree
 {
     /**
-     * The parent CHM instance.
-     *
-     * @var CHM
-     */
-    protected $chm;
-
-    /**
      * List of Item instances children of this tree.
      *
      * @var Item[]
@@ -31,9 +24,8 @@ class Tree
     /**
      * Initializes the instance.
      */
-    protected function __construct(CHM $chm)
+    public function __construct()
     {
-        $this->chm = $chm;
         $this->items = array();
     }
 
@@ -45,6 +37,16 @@ class Tree
     public function getItems()
     {
         return $this->items;
+    }
+
+    /**
+     * Append to the children of this tree the children of another tree.
+     *
+     * @param Tree $tree
+     */
+    public function mergeItems(Tree $tree)
+    {
+        $this->items = array_merge($this->items, $tree->items);
     }
 
     /**
@@ -70,28 +72,6 @@ class Tree
     }
 
     /**
-     * Create a new instance starting from an UL element.
-     *
-     * @param CHM $chm The parent CHM instance.
-     * @param DOMElement $ul The UL element to be parsed.
-     *
-     * @throws Exception Throw an Exception in case of errors.
-     *
-     * @return static
-     */
-    public static function fromUL(CHM $chm, DOMElement $ul)
-    {
-        $result = new static($chm);
-        foreach ($ul->childNodes as $li) {
-            if ($li instanceof DOMElement && strcasecmp($li->tagName, 'li') === 0) {
-                $result->items = array_merge($result->items, Item::fromLI($chm, $li));
-            }
-        }
-
-        return $result;
-    }
-
-    /**
      * Create a new instance starting from the whole TOC/Index source 'HTML'.
      *
      * @param CHM $chm The parent CHM instance.
@@ -106,40 +86,69 @@ class Tree
         if (!class_exists('DOMDocument', false) || !class_exists('DOMXpath', false)) {
             throw new Exception('Missing PHP extension: php-xml');
         }
+        $result = new static();
         $data = trim((string) $data);
-        $doc = new DOMDocument();
-        $charset = 'UTF-8';
-        if (preg_match('/^<\?xml\s+encoding\s*=\s*"([^"]+)"/i', $data, $m)) {
-            $charset = $m[1];
-        } else {
-            if (preg_match('/<meta\s+http-equiv\s*=\s*"Content-Type"\s+content\s*=\s*"text\/html;\s*charset=([^"]+)">/i', $data, $m)) {
+        if (stripos($data, '<object') !== false) {
+            $doc = new DOMDocument();
+            $charset = 'UTF-8';
+            if (preg_match('%^<\?xml\s+encoding\s*=\s*"([^"]+)"%i', $data, $m)) {
                 $charset = $m[1];
+            } else {
+                if (preg_match('%<meta\s+http-equiv\s*=\s*"Content-Type"\s+content\s*=\s*"text/html;\s*charset=([^"]+)">%i', $data, $m)) {
+                    $charset = $m[1];
+                }
+                $data = '<?xml encoding="'.$charset.'">'.$data;
             }
-            $data = '<?xml encoding="'.$charset.'">'.$data;
-        }
-        if (@$doc->loadHTML($data) !== true) {
-            throw new Exception('Failed to parse the .hhc/.hhk file contents');
-        }
-        $xpath = new DOMXpath($doc);
-        $elements = $xpath->query('/html/body/ul');
-        switch ($elements->length) {
-            case 0:
-                $elements = $xpath->query('/html/body/object[@type="text/sitemap"]');
-                if ($elements->length === 0) {
-                    throw new Exception('No root list in the .hhc/.hhk file contents');
-                }
-                $result = new static($chm);
-                foreach ($elements as $object) {
-                    $result->items[] = Item::fromObject($chm, $object);
-                }
-                break;
-            case 1:
-                $result = static::fromUL($chm, $elements->item(0));
-                break;
-            default:
-                throw new Exception('More that one root list in the .hhc/.hhk file contents');
+            // LI elements are very often malformed/misplaced: let's remove them
+            $data = preg_replace('$</?li((\\s+[^>]*)|\\s*)>$i', '', $data);
+            if (@$doc->loadHTML($data) !== true) {
+                throw new Exception('Failed to parse the .hhc/.hhk file contents');
+            }
+            $result->parseParentElement($chm, $doc->documentElement, 0);
         }
 
         return $result;
+    }
+
+    /**
+     * Depth of the found child items.
+     *
+     * @var int
+     */
+    protected $depth;
+
+    /**
+     * Parse a DOMElement and read the items/sub trees.
+     *
+     * @param CHM $chm
+     * @param DOMElement $parentElement
+     * @param int $depth
+     */
+    protected function parseParentElement(CHM $chm, DOMElement $parentElement, $depth)
+    {
+        foreach ($parentElement->childNodes as $node) {
+            if ($node->nodeType === XML_ELEMENT_NODE) {
+                switch (strtolower($node->tagName)) {
+                    case 'object':
+                        if (strtolower($node->getAttribute('type')) === 'text/sitemap') {
+                            $this->depth = $depth;
+                            $this->items[] = new Item($chm, $node);
+                        }
+                        break;
+                    case 'ul':
+                    case 'ol':
+                        $n = count($this->items);
+                        if ($n > 0 && $depth >= $this->depth) {
+                            $this->items[$n - 1]->getChildren()->parseParentElement($chm, $node, $depth + 1);
+                        } else {
+                            $this->parseParentElement($chm, $node, $depth + 1);
+                        }
+                        break;
+                    default:
+                        $this->parseParentElement($chm, $node, $depth + 1);
+                        break;
+                }
+            }
+        }
     }
 }
