61efcf4ec29a386301a0bc967d811f04249196c7
Michele Locati <michele@locati.it>
2016-05-06 15:06:56 +0200

    Add method FileReader::getFilename

--- src/Reader/FileReader.php
+++ src/Reader/FileReader.php
@@ -69,6 +69,16 @@ class FileReader extends Reader
     }
 
     /**
+     * Get the file name.
+     *
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
      * {@inheritdoc}
      *
      * @see Reader::setPosition()
